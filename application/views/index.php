9ef7e53b645b776e5a5259e47ccc28ad50bf8acc
dolimoni <khalid.essalhi8@gmail.com>
2015-07-28 23:39:12 +0100

    by dolimoni

---
+++ application/views/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<h2>Bienvenue dans le site le l'ENSA</h2>
+<a href="<?php echo site_url('index/inscription/') ?>">inscription</a>
+</body>
+</html>
---
+++ application/views/inscription.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<form method="post" action="<?= site_url('inscription') ?>">
+	nom : <input type="text" name="nom"/>
+</form>
+</body>
+</html>
