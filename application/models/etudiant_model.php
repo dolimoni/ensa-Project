9ed9471edd349f42e87754bb4f81296c2ee3fb7b
Mohamed Essaidi <essaidi1@gmail.com>
2015-07-29 22:30:21 +0100

    isValid() fct - model Etudiant

---
+++ application/models/etudiant_model.php
@@ -0,0 +1,32 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Etudiant_model extends CI_Model
+{
+	protected $table = 'etudiant';
+	
+	/**
+	 *	check if user is valid
+     *  return true if valid , else return false
+	 */
+    private function isValidUser($nom,$prenom,$cin,$cne){
+        if($nom == "" || $prenom == "" || $cin == "" || $cne == ""){
+            return false;
+        }
+        
+        $query = $this->db->select("id")
+                    ->from($this->table)
+                    ->where('nom',strtolower($nom))
+                    ->where('prenom',strtolower($prenom))
+                    ->where('cin',strtolower($cin))
+                    ->where('cne',strtolower($cne))
+                    ->get();
+        
+        if ( $query->num_rows() > 0 )
+        {
+            return true;
+        }else{
+            return false;
+        }
+    }
+	
+}
