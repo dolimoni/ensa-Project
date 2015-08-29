6a4a688d8e8d1b0d8c9d573ca678f7c8cf8e8169
Mohamed Essaidi <essaidi1@gmail.com>
2015-08-29 23:47:56 +0100

    Change Style of index page - Edit

--- application/views/index.php
+++ application/views/index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Bynatna.ma - Inscription</title>
+        <title>ENSA - Authentification</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="icon" type="image/png" href="images/favicon-32x32.png" />
