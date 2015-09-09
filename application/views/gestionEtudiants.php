84e5ea82528600b7d346482c9c74eed2748999b1
Mohamed Essaidi <essaidi1@gmail.com>
2015-09-09 21:36:16 +0100

    Edit HTML of Administration

--- application/views/gestionEtudiants.php
+++ application/views/gestionEtudiants.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>ENSA - Gestion des etudiants</title>
+        <title>ENSA - Administration</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="icon" type="image/png" href="images/favicon-32x32.png" />
@@ -14,47 +14,63 @@
         
         <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/signup.css" />
         <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/login.css" />
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/admin.css"/>
     </head>
     <body>
         <!-- Inscription -->
         <div id="e_signup">
             <div class="container">
                 <div class="e_title col-sm-4 col-sm-offset-2">
-                    <h3>Gestion des etudiants</h3>
+                    <h3>Administration</h3>
                 </div>
-                <div>
-                <table class="table-list-etudiants table table-striped">
-                <thead>
-                  <tr>
-                    <th>#</th>
-                    <th>Nom</th>
-                    <th>Prenom</th>
-                    <th>CNE</th>
-                    <th>CIN</th>
-                    <th>Outils</th>
-                  </tr>
-                </thead>
-                <tbody>
-                    <?php
-                    foreach ($etudiants as $etudiant){
-                    ?>
-                    <tr>
-                        <th scope="row"><?php echo $etudiant["id"] ?></th>
-                        <td><?php echo $etudiant["nom"] ?></td>
-                        <td><?php echo $etudiant["prenom"] ?></td>
-                        <td><?php echo $etudiant["cne"] ?></td>
-                        <td><?php echo $etudiant["cin"] ?></td>
-                        <td>
-                            <a href="#" title="Afficher le profile" class="btn btn-success"><span class="glyphicon glyphicon-eye-open"></span></a>
-                            <a href="<?php echo site_url("admin_controller/editProfile/".$etudiant["id"]); ?>" title="Modifier les information" class="btn btn-info"><span class="glyphicon glyphicon-edit"></span></a>
-                            <a href="<?php echo site_url("admin_controller/supprimerEtudiant/".$etudiant["id"]); ?>" title="Supprimer" class="btn btn-danger btn-delete"><span class="glyphicon glyphicon-remove"></span></a>
-                        </td>
-                    </tr>
-                    <?php
-                    }
-                    ?>
-                </tbody>
-              </table>
+                <div class="row admin">
+                    <div class="col-sm-3 sidebar">
+                        <ul>
+                            <li><a href="<?php echo site_url("Admin"); ?>"><span class="glyphicon glyphicon-dashboard"></span> Information generale</a></li>
+                            <li><a href="<?php echo site_url("admin_controller/gestionEtudiants"); ?>"><span class="glyphicon glyphicon-user"></span> Gestion des etudiants</a></li>
+                            <li><a href="<?php echo site_url("Admin/adsManagement"); ?>"><span class="glyphicon glyphicon-picture"></span> Gestion des fillieres</a></li>
+                            <li><a href="<?php echo site_url("admin_controller/statistics"); ?>"><span class="glyphicon glyphicon-file"></span> Statistiques</a></li>
+                            <li><a href="<?php echo site_url("admin_controller/attribution"); ?>"><span class="glyphicon glyphicon-file"></span> Attribution des filières</a></li>
+                            <li><a href="<?php echo site_url("etudiant_controller/deconnexion"); ?>"><span class="glyphicon glyphicon-file"></span> Deconnexion</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-sm-9 content">
+                        <h3 class="page-title">Dashboard <span> Gestion des etudiants </span></h3>
+                        <div>
+                            <table class="table-list-etudiants table table-striped">
+                                <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Nom</th>
+                                        <th>Prenom</th>
+                                        <th>CNE</th>
+                                        <th>CIN</th>
+                                        <th>Outils</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php
+                                    foreach ($etudiants as $etudiant){
+                                    ?>
+                                    <tr>
+                                        <th scope="row"><?php echo $etudiant["id"] ?></th>
+                                        <td><?php echo $etudiant["nom"] ?></td>
+                                        <td><?php echo $etudiant["prenom"] ?></td>
+                                        <td><?php echo $etudiant["cne"] ?></td>
+                                        <td><?php echo $etudiant["cin"] ?></td>
+                                        <td>
+                                            <a href="#" title="Afficher le profile" class="btn btn-success"><span class="glyphicon glyphicon-eye-open"></span></a>
+                                            <a href="<?php echo site_url("admin_controller/editProfile/".$etudiant["id"]); ?>" title="Modifier les information" class="btn btn-info"><span class="glyphicon glyphicon-edit"></span></a>
+                                            <a href="<?php echo site_url("admin_controller/supprimerEtudiant/".$etudiant["id"]); ?>" title="Supprimer" class="btn btn-danger btn-delete"><span class="glyphicon glyphicon-remove"></span></a>
+                                        </td>
+                                    </tr>
+                                    <?php
+                                    }
+                                    ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
